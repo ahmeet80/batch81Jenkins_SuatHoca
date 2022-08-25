00ed0e1f6207d0dd886fa9f26ecf5d0665a9eb41
Suat ORUÇ <93098605+suatoruc@users.noreply.github.com>
2022-08-25 13:40:09 +0300

    Update UIRunner.java

--- src/test/java/ui/runners/UIRunner.java
+++ src/test/java/ui/runners/UIRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin={"html:target\\cucumber-reports.html",
-               // "json:target/json-reports/cucumber.json",
+                "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"},
         features="src/test/resources/uiFeatures",
         glue="ui/stepdef",
