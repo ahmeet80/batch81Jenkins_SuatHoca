4a4f646ce756475f0db70cd25e9c3c29c7eddf74
Suat <suattoruc@gmail.com>
2022-08-25 15:22:14 +0300

    forJenks3

--- src/test/java/ui/runners/UIRunner.java
+++ src/test/java/ui/runners/UIRunner.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin={"pretty", "html:target/default-cucumber-reports"},
+        plugin={"pretty","html:target\\cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
         features="src/test/resources/uiFeatures",
         glue="ui/stepdef",
         tags="@a"  ,
